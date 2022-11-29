61359df5e7241a0aee80c010a1499dc07b71acfe
Rimon Moomey <rimon.moomey@gmail.com>
2022-11-29 13:48:54 -0600

    complete challenge and type up solution

--- javascript/fibonacci_recursive.js
+++ javascript/fibonacci_recursive.js
@@ -1,5 +1,11 @@
 function fibonacci(n) {
-  // type your code here
+  if (n===0) {
+    return 0;
+  } else if (n===1) {
+    return 1;
+  } else {
+    return fibonacci(n-2) + fibonacci(n-1);
+  }
 }
 
 if (require.main === module) {
@@ -22,3 +28,16 @@ module.exports = fibonacci;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/**
+ * The n^th fibonacci number is mathematically defined to be equal to the sum of the (n-2)^th and (n-1)^th fibonacci numbers.
+ * As such, our recursive function simply needs to recursively add the fibonacci numbers together until arriving, at the end of each stack,
+ * at the 0th or 1st fibonacci numbers, which are defined to be 0 and 1, respectively.
+ * 
+ * pseudocode
+ * 
+ * function fibonacci(n) {
+ * if n is 1, return 0
+ * else if n is 2, return 1
+ * else return fibonacci(n - 2) + fibonacci(n - 1)
+ * }
+ */
